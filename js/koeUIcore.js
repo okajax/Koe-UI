7202b0ff75f399b5ac79e543da2eb9a08a10bae6
TakenoriOkada <okada@okada-no-MacBook-Pro.local>
2016-06-04 12:17:30 +0900

    ui-simple 完成

--- js/koeUIcore.js
+++ js/koeUIcore.js
@@ -1,14 +1,15 @@
 var koeUIcore = {
 
-    // js-cookie依存
+    // * js-cookie依存
+
+    // UIのタイプの如何に関わらず、共通する機能をここに格納する
 
     // ***********************************************************
     // 変数
     // ***********************************************************
-    convID: null,
-    init_d: null,
-    //token: this.opts.token,
-
+    convID: null, // カンバセーションID: DirectLineAPIが、会話を記録するためのID。30分で無効になる
+    watermark: null, // ウォーターマーク: この指定をすると、新しい発言だけ取得できる
+    //msgObj: null, // メッセージを格納するオブジェクト
 
     // ***********************************************************
     // カンバセーションIDの判定・作成をします
@@ -16,9 +17,7 @@ var koeUIcore = {
 
     init: function () {
 
-        var token = this.opts.token;
-
-        var that = this;
+        var that = this; // thatはRiotのカスタムタグを指す
         var cookie_ConvID = Cookies.get('convID');
 
         // クッキーにカンバセーションIDがすでにあるか調べる
@@ -28,20 +27,18 @@ var koeUIcore = {
 
             // ヘッダーを指定
             var myHeaders = new Headers();
-            myHeaders.append('Authorization', 'Botconnector ' + token);
-
+            myHeaders.append('Authorization', 'Botconnector ' + this.opts.token);
             // オプションを指定
             var myInit = {
                 method: 'POST',
                 headers: myHeaders,
                 contentType: 'application/json'
             };
-
             // リクエストを指定
             var myRequest = new Request('https://directline.botframework.com/api/conversations', myInit);
 
             // fetch開始だぽっぽ
-            that.init_d = fetch(myRequest)
+            fetch(myRequest)
                 .then(function (data) {
                     return data.json() // 紛らわしいからJSONにするぽっぽ
                 })
@@ -67,5 +64,86 @@ var koeUIcore = {
             //that.update();
 
         }
+    },
+    getMessage: function (convID, watermark) {
+
+        console.log('getMessage');
+
+        if (convID == undefined) {
+            console.log('カンバセーションIDがありません');
+            return false;
+        }
+
+        var that = this; // thatはRiotのカスタムタグを指す
+        var get_url;
+
+        //ウォーターマーク指定があるか調べる
+        if (watermark) {
+            // 指定があれば、ウォーターマーク付きのURLでGETする。
+            get_url = 'https://directline.botframework.com/api/conversations/' + convID + '/messages?watermark=' + watermark;
+        } else {
+            // なければそのまま。ウォーターマークがなければ、全てのメッセージを取得する。
+            get_url = 'https://directline.botframework.com/api/conversations/' + convID + '/messages';
+        }
+
+        // ヘッダーを指定
+        var myHeaders = new Headers();
+        myHeaders.append('Authorization', 'Botconnector ' + this.opts.token);
+        // オプションを指定
+        var myInit = {
+            method: 'GET',
+            headers: myHeaders,
+            contentType: 'application/json'
+        };
+        // リクエストを指定
+        var myRequest = new Request(get_url, myInit);
+
+        // fetch開始だぽっぽ
+        fetch(myRequest)
+            .then(function (data) {
+                return data.json() // 紛らわしいからJSONにするぽっぽ
+            })
+            .then(function (response) {
+
+                // すでに格納しているwatermarkを更新
+                that.watermark = response.watermark;
+
+                // 取得した会話を格納
+                that.msgObj = response;
+
+                 that.update();
+
+                console.log(response);
+                //return response.messages;
+
+            });
+
+    },
+    sendMessage: function (convID, text, from) {
+
+        console.log(text);
+        console.log('sendMessage');
+
+        var send_url = 'https://directline.botframework.com/api/conversations/' + convID + '/messages';
+
+        // オプションを指定
+        var myInit = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Botconnector ' + this.opts.token
+            },
+            body: JSON.stringify({
+                'text': text
+            })
+        };
+        // リクエストを指定
+        var myRequest = new Request(send_url, myInit);
+
+        // fetch開始だぽっぽ
+        fetch(myRequest);
+
     }
+
+
 };
